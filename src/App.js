05dc1b0cc4669eb081535b1c2d8645043282a4fd
Sorbopriyo Roy <sorbopriyo@gmail.com>
2023-10-27 11:09:33 +0530

    Accuracy feature

--- src/App.js
+++ src/App.js
@@ -1,45 +1,54 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import TypingText from "./TypingText";
+import Label from "./components/Label";
 
 function App() {
   const [quote] = useState(
     "Ion propulsion, also known as ion thrusters or ion drives, is a type of propulsion technology used in space exploration. Unlike traditional chemical rocket engines that rely on the combustion of propellants, ion propulsion systems use electric fields to accelerate and expel charged particles, called ions, to generate thrust."
   );
   const quoteWords = quote.split(" ");
+  const quoteChar = quote.split("");
   const [typedText, setTypedText] = useState("");
   const [startTime, setStartTime] = useState(null);
-  const [typingSpeed, setTypingSpeed] = useState(null);
+  const [typingSpeed, setTypingSpeed] = useState(0);
+  const [accuracy, setAccuracy] = useState(0);
 
   useEffect(() => {
     if (typedText.length === 1) {
       setStartTime(new Date());
       setTypingSpeed(0);
+      setAccuracy(0);
     }
 
+    const typedChar = typedText.split("");
     const typedWords = typedText.split(" ");
-    let correctWords = 0;
+    let correctChar = 0;
 
-    for (let i = 0; i < typedWords.length; i++) {
-      if (typedWords[i] === quoteWords[i]) {
-        correctWords++;
+    for (let i = 0; i < typedChar.length; i++) {
+      if (typedChar[i] === quoteChar[i]) {
+        correctChar++;
       }
     }
 
-    if (correctWords === quoteWords.length) {
+    if (typedChar.length === quoteChar.length) {
       const elapsedTime = (new Date() - startTime) / 1000;
-      const wordsTyped = quoteWords.length;
+      const wordsTyped = typedWords.length;
       const speed = Math.floor((wordsTyped / elapsedTime) * 60);
+      const acc = Math.floor((correctChar / quoteChar.length) * 100);
       setTypingSpeed(speed);
+      setAccuracy(acc);
     }
-  }, [typedText, startTime, quoteWords]);
+  }, [typedText, startTime, quoteWords, quoteChar]);
 
   return (
     <main>
       <h1>Typing Speed Test</h1>
+
       <div className="typing-text" id="quote">
         <TypingText text={typedText} quote={quote} />
       </div>
+
       <textarea
         className="typing-box"
         id="typing-box"
@@ -47,10 +56,12 @@ function App() {
         value={typedText}
         onChange={(e) => setTypedText(e.target.value)}
       ></textarea>
+
       <div className="results">
-        Typing Speed: <span id="typing-speed">{typingSpeed || 0}</span> WPM
-        (Words Per Minute)
+        <Label text={`${typingSpeed} WPM`} />
+        <Label text={`${accuracy}%`} />
       </div>
+
       <footer>Created by Sorbopriyo Roy in 2023</footer>
     </main>
   );
---
+++ src/components/Label.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Label({ text }) {
+  return <div className="label">{text}</div>;
+}
